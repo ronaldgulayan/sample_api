a7ac779c4770a81ea25008ced05e67d70a131897
Ronald Gulayan <ronaldgulayan00@gmail.com>
2023-11-19 12:11:18 +0800

    change host

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 const PORT = 8081;
 const db = mysql.createConnection({
-  host: "localhost",
+  host: "64.224.96.84",
   user: "ron",
   password: "ron02",
   database: "sample",
