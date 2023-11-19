17fe8f5227a351c73acb8b1a956fd35ce140f714
Ronald Gulayan <ronaldgulayan00@gmail.com>
2023-11-19 11:13:49 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mysql = require("mysql");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const PORT = 3301;
+const db = mysql.createConnection({
+  host: "64.224.96.84",
+  user: "ron",
+  password: "ron02",
+  database: "sample",
+});
+
+app.get("/api/get_info", (req, res) => {
+  const sql = "SELECT * FROM info;";
+
+  db.query(sql, function (error, result) {
+    if (error) return res.json({ status: "error", data: error });
+    return res.json({ status: "success", data: result });
+  });
+});
+
+app.listen(PORT, () => {
+  console.log("Server is now running on port " + PORT);
+});
